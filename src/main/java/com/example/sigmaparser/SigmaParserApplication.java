4e1980c740c511dc736c30fa3abec52df94fd567
Тимченко Влад <timchenko.vld@gmail.com>
2024-04-08 13:25:44 +0300

    Initial commit

---
+++ src/main/java/com/example/sigmaparser/SigmaParserApplication.java
@@ -0,0 +1,13 @@
+package com.example.sigmaparser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SigmaParserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SigmaParserApplication.class, args);
+	}
+
+}
